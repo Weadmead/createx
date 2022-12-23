50d7bb70e27ff5ffc8fef70da93df47065a2d8a5
Rodion <wead758@gmail.com>
2022-12-24 02:30:40 +0300

    added section benefits

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,13 @@
 $(function () {
 
+    $('.benefits__tab-item').on('click', function (e) {
+        e.preventDefault();
+        $('.benefits__tab-item').removeClass('benefits__tab-item--active');
+        $(this).addClass('benefits__tab-item--active');
+
+        $('.benefits__content-item').removeClass('benefits__content-item--active');
+        $($(this).attr('href')).addClass('benefits__content-item--active');
+    });
+
+
 });
