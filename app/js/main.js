8b9a5fbcda8036694573cf9d3947c3f1945c2cb8
Rodion <wead758@gmail.com>
2022-12-29 06:59:48 +0300

    added section discount

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,48 @@
 $(function () {
 
+    function getTimeRemaining(endtime) {
+        const total = Date.parse(endtime) - Date.parse(new Date());
+        const seconds = Math.floor((total / 1000) % 60);
+        const minutes = Math.floor((total / 1000 / 60) % 60);
+        const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+        const days = Math.floor(total / (1000 * 60 * 60 * 24));
+        
+        return {
+          total,
+          days,
+          hours,
+          minutes,
+          seconds
+        };
+      }
+      
+      function initializeClock(id, endtime) {
+        const clock = document.getElementById(id);
+        const daysSpan = clock.querySelector('.discount__counter-days');
+        const hoursSpan = clock.querySelector('.discount__counter-hours');
+        const minutesSpan = clock.querySelector('.discount__counter-mins');
+        const secondsSpan = clock.querySelector('.discount__counter-sec');
+      
+        function updateClock() {
+          const t = getTimeRemaining(endtime);
+      
+          daysSpan.innerHTML = t.days;
+          hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+          minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+          secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+      
+          if (t.total <= 0) {
+            clearInterval(timeinterval);
+          }
+        }
+      
+        updateClock();
+        const timeinterval = setInterval(updateClock, 1000);
+      }
+      
+      const deadline = $('.discount__counter-list').attr('data-time');
+      initializeClock('counter', deadline);
+
 
 
     //slider section team
